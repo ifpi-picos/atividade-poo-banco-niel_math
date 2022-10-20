5b5839717e0ffd17df1e6bb15102e97e09a636be
Your Name <you@example.com>
2022-10-20 19:56:05 -0300

    sem alteracao

--- banco_sistema_financeiro/src/App.java
+++ banco_sistema_financeiro/src/App.java
@@ -9,40 +9,42 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Qual Método de acesso: (0) até (2)\n");
+        System.out.println("Qual Metodo de acesso: (0) ate (2)\n");
         System.out.println("| 1 | - Criar nova conta \n| 2 | - Entrar na conta \n| 0 | - Sair\n");
         int method = scanner.nextInt();
 
         if (method == 0) {
             System.out.println("Programa Finalizado.");
         } else if (method == 1) {
-            System.out.println("Nome: ");
-            Cliente c1 = new Cliente((scanner.nextLine()), null, null, null, null);
-
+            Cliente cliente1 = new Cliente();
+            System.out.println("Digite o nome: ");
+            String nomedocliente = scanner.nextLine();
+            cliente1.setNome(nomedocliente);
+     
         } else if (method == 2) {
 
         } else {
             System.out.println("Número não corresponde");
         }
 
-        Endereco endereco = new Endereco("Pics", "Sao Jose", "Rua de zeze", 21);
-        Poupanca poupanca = new Poupanca(4893627, 026, 1000.00, null);
-        Corrente corrente = new Corrente(5427272, 054, 2000.00, null, 500.00);
-        Cliente cliente = new Cliente("Zeze", "514.297.801-51", LocalDate.of(2000, 1, 15), endereco, corrente);
-        Cliente cliente2 = new Cliente("teus", "514.291.941-15", LocalDate.of(2000, 1, 15), endereco, poupanca);
+        //Endereco endereco = new Endereco("Pics", "Sao Jose", "Rua de zeze", 21);
+        //Poupanca poupanca = new Poupanca(4893627, 026, 1000.00, null);
+        //Corrente corrente = new Corrente(5427272, 054, 2000.00, null, 500.00);
+        //Cliente cliente = new Cliente("Zeze", "514.297.801-51", LocalDate.of(2000, 1, 15), endereco, corrente);
+        //Cliente cliente2 = new Cliente("teus", "514.291.941-15", LocalDate.of(2000, 1, 15), endereco, poupanca);
 
-        System.out.print("total conta poupanca = ");
-        System.out.println(poupanca.getSaldo());
-        System.out.print("total conta corrente = ");
-        System.out.println(corrente.getSaldo());
+        //System.out.print("total conta poupanca = ");
+        //System.out.println(poupanca.getSaldo());
+        //System.out.print("total conta corrente = ");
+        //System.out.println(corrente.getSaldo());
 
         // conta.transferir(conta2, 100.00);
 
         // System.out.println(conta2.getSaldo());
         // System.out.println(conta.getSaldo());
 
-        poupanca.sacar(1500.00);
+        //poupanca.sacar(1500.00);
 
-        System.out.println(poupanca.getSaldo());
+        //System.out.println(poupanca.getSaldo());
     }
 }
