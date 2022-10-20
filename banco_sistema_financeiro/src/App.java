51b1c63b4ecdf0472859809afcbda95b1072c31c
Jose-Nataniel <capic.2020118tads0339@aluno.ifpi.edu.br>
2022-10-20 15:36:51 -0300

    adding scanner

--- banco_sistema_financeiro/src/App.java
+++ banco_sistema_financeiro/src/App.java
@@ -2,12 +2,29 @@ import java.util.Date;
 import br.edu.ifpi.dominio.*;
 import java.time.LocalDate;
 import java.util.Scanner;
+
 // Adicionar scanner completo.
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.println("Qual Método de acesso: (0) até (2)\n");
+        System.out.println("| 1 | - Criar nova conta \n| 2 | - Entrar na conta \n| 0 | - Sair\n");
+        int method = scanner.nextInt();
+
+        if (method == 0) {
+            System.out.println("Programa Finalizado.");
+        } else if (method == 1) {
+            System.out.println("Nome: ");
+            Cliente c1 = new Cliente((scanner.nextLine()), null, null, null, null);
+
+        } else if (method == 2) {
+
+        } else {
+            System.out.println("Número não corresponde");
+        }
+
         Endereco endereco = new Endereco("Pics", "Sao Jose", "Rua de zeze", 21);
         Poupanca poupanca = new Poupanca(4893627, 026, 1000.00, null);
         Corrente corrente = new Corrente(5427272, 054, 2000.00, null, 500.00);
@@ -19,10 +36,10 @@ public class App {
         System.out.print("total conta corrente = ");
         System.out.println(corrente.getSaldo());
 
-        //conta.transferir(conta2, 100.00);
+        // conta.transferir(conta2, 100.00);
 
-        //System.out.println(conta2.getSaldo());
-        //System.out.println(conta.getSaldo());
+        // System.out.println(conta2.getSaldo());
+        // System.out.println(conta.getSaldo());
 
         poupanca.sacar(1500.00);
 
