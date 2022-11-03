5a0cf361f3a9dd21edff7840b18187686f37b1f6
Your Name <you@example.com>
2022-11-03 20:03:43 -0300

    taxa

--- banco_sistema_financeiro/src/App.java
+++ banco_sistema_financeiro/src/App.java
@@ -3,7 +3,6 @@ import br.edu.ifpi.dominio.*;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-// Adicionar scanner completo.
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -32,30 +31,32 @@ public class App {
                         Cliente cliente = new Cliente(null, null, null, endereco, null);
                         Corrente corrente = new Corrente(123, 456, 1000.00, cliente, 1000.00);
 
-                        System.out.print(" Endereço: ");
-                        System.out.println("Cidade: ");
+                        System.out.println("Endereço: ");
+                        System.out.print("Cidade: ");
                         endereco.setCidade(scanner.nextLine());
-                        System.out.println("Bairro: ");
+                        endereco.setCidade(scanner.nextLine());
+                        System.out.print("Bairro: ");
                         endereco.setBairro(scanner.nextLine());
-                        System.out.println("Rua: ");
+                        System.out.print("Rua: ");
                         endereco.setRua(scanner.nextLine());
-                        System.out.println("Numero: ");
+                        System.out.print("Numero: ");
                         endereco.setNumero(scanner.nextInt());
 
-                        System.out.print(" Cliente: ");
-                        System.out.println("Nome: ");
+                        System.out.println(" Cliente: ");
+                        System.out.print("Nome: ");
                         cliente.setNome(scanner.nextLine());
-                        System.out.println("Cpf: ");
+                        cliente.setNome(scanner.nextLine());
+                        System.out.print("Cpf: ");
                         cliente.setCpf(scanner.nextLine());
-                        System.out.println("Data de  Nascimento: ");
+                        System.out.print("Data de  Nascimento: ");
                         cliente.setDataNasc(scanner.nextLine());
                         cliente.setEndereco(endereco);
 
-                        System.out.print(" Conta Corrente: ");
-                        System.out.format("%d", corrente.getNumeroAg());
-                        System.out.format("%d", corrente.getNumeroCo());
-                        System.out.format("%d", corrente.getSaldo());
-                        System.out.format("%d", corrente.getChequeEs());
+                        System.out.println("Conta Corrente: ");
+                        System.out.format("%d\n", corrente.getNumeroAg());
+                        System.out.format("%d\n", corrente.getNumeroCo());
+                        System.out.format("%.2f\n", corrente.getSaldo());
+                        System.out.format("%.2f\n", corrente.getChequeEs());
 
                         break;
                     }
@@ -76,7 +77,7 @@ public class App {
                         System.out.print("Numero: ");
                         endereco.setNumero(scanner.nextInt());
 
-                        System.out.println("Cliente ");
+                        System.out.println("Cliente");
                         System.out.print("Nome: ");
                         cliente.setNome(scanner.nextLine());
                         cliente.setNome(scanner.nextLine());
@@ -86,10 +87,10 @@ public class App {
                         cliente.setDataNasc(scanner.nextLine());
                         cliente.setEndereco(endereco);
 
-                        System.out.print(" Conta Corrente: ");
-                        System.out.format("%d", poupanca.getNumeroAg());
-                        System.out.format("%d", poupanca.getNumeroCo());
-                        System.out.format("%d", poupanca.getSaldo());
+                        System.out.print("Conta Poupança: ");
+                        System.out.format("%d\n", poupanca.getNumeroAg());
+                        System.out.format("%d\n", poupanca.getNumeroCo());
+                        System.out.format("%f\n", poupanca.getSaldo());
 
                         break;
                     }
--- banco_sistema_financeiro/src/br/edu/ifpi/dominio/Corrente.java
+++ banco_sistema_financeiro/src/br/edu/ifpi/dominio/Corrente.java
@@ -1,23 +1,41 @@
 package br.edu.ifpi.dominio;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Corrente extends Conta {
     private double chequeEs;
+    private int qtdTransferecia;
+    private double taxa = 0.05;
 
     public Corrente(int numeroAg, int numeroCo, double saldo, Cliente cliente1, double chequeEs) {
         super(numeroAg, numeroCo, saldo, cliente1);
         this.chequeEs = chequeEs;
     }
 
-    //a gente tem que fazer pra retornar o valor.
     @Override
     public void sacar(double valor) {
-        if (this.getSaldo() + this.getChequeEs() >= valor);
-        return ;//aqui vai algo
+        if (this.getSaldo() + this.getChequeEs() >= valor) {
+            System.out.println("Saldo Insuficiente.");
+        } else {
+            this.setSaldo(this.getSaldo() - valor);
+        }
     }
 
     @Override
     public void transferir(Conta conta2, double valor) {
-        super.transferir(conta2, valor);
+        double valorRetirado = qtdTransferecia < 3 ? valor : valor + (valor * taxa);
+        
+        if (valorRetirado <= getChequeEs() + getSaldo()) {
+            if (qtdTransferecia < 3) {
+                super.transferir(conta2, valor);
+            } else {
+                super.saldo = super.saldo - valorRetirado;
+                conta2.depositar(valor);
+            }
+            qtdTransferecia +=1;
+        } else {
+            System.out.println("sem saldo");
+        }
     }
 
     public void setChequeEs(double chequeEs) {
